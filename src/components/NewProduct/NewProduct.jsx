8975240db1f122c7914a3bf05af0aeb9b24bb5a6
noemi <noemibonilla95@hotmail.com>
2024-09-09 08:06:46 +0200

    nuevo componente newproduct.jsx y newproduct.css contiene principalmente la introduccion y la inserccion de cada elemento en la area productos nuevos --shop

---
+++ src/components/NewProduct/NewProduct.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./NewProduct.css"
+import data_product from '../Assets/svg/data'
+import Items from "../Items/Items"
+
+export default function NewProduct() {
+  return (
+    <div className='newProducts'>
+
+      <div className="new-products">
+            <h1>
+        Nuevos Productos
+      </h1>
+      <hr/>
+      </div>
+      <div className="item-product">
+        {data_product.map((item, i)=> {
+            return <Items key={i} id= {item.id} name= {item.name} image= {item.image} price={item.price} />
+        })}
+      </div>
+    </div>
+  )
+}
