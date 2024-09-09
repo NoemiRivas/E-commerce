a0457f1d4aabe43a9bb3699e929329f31c7894de
noemi <noemibonilla95@hotmail.com>
2024-09-09 06:03:26 +0200

    datos de inicio para hacer la pagina---debagregar los datos pricipales mas adelante

---
+++ src/components/Assets/svg/data.js
@@ -0,0 +1,32 @@
+import reloj from "../../../Assets/img/reloj.png"
+import anillo from "../../../Assets/img/anillo.png"
+import balanza from "../../../Assets/img/balanza.png"
+//productos de ejemplo
+
+let data_product = [
+    {
+        id:1,
+        name:"Thermal Running Watch Element 2.0",
+        image:reloj,
+        price:259
+
+    },
+    {
+        id:2,
+        name:"anillo inteligente",
+        image:anillo,
+        price:289
+
+    },
+    {
+        id:1,
+        name:"Balanza Digital Bluetooth",
+        image:balanza,
+        price:49
+
+    }
+
+
+]
+
+export default data_product;
