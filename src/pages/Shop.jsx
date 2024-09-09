4610e8fffdd1c82df7c83619c0883a5f215d8517
noemi <noemibonilla95@hotmail.com>
2024-09-09 06:16:59 +0200

    paginas de la tienda muestra los productos

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero'
+import NewProduct from '../components/NewProduct/NewProduct'
+
+export default function Shop() {
+  return (
+    <div>
+      <Hero/>
+      <NewProduct/>
+    </div>
+  )
+}
