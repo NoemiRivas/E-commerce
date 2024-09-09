638b0cdf03c1b94ff4d3320e5a5f305de6ab6688
noemi <noemibonilla95@hotmail.com>
2024-09-09 08:04:03 +0200

    nuevo componente item.jsx y item.css contienen las imagenesprecio y botones a mostrar

---
+++ src/components/Items/Items.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Item.css"
+//mostrar cada producto
+export default function Items(props) {
+  return (
+    <div className='item'>
+      <img src= {props.image} alt="" />
+      <p> {props.name} </p>
+      <div className='item-price'>
+        ${props. price}
+      </div>
+      <button className="btn-price">
+        Comprar
+      </button>
+    </div>
+  )
+}
