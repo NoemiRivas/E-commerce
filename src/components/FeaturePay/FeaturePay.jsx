30439e2a88035e48cbbc173103b77605a8a52955
noemi <noemibonilla95@hotmail.com>
2024-09-11 22:03:33 +0200

    actualizacion

--- src/components/FeaturePay/FeaturePay.jsx
+++ src/components/FeaturePay/FeaturePay.jsx
@@ -9,18 +9,18 @@ export default function FeaturePay() {
   return (
     <div className='features'>
       <div className='delivery'>
-        <DeliveryIcon style={{ color: "#fff" }} />
+        <DeliveryIcon/>
         <h2>Conseña gratuita</h2>
         <p className='price-delivery'>"Únicamente para ordenes superiores a 59$"</p>
       </div>
       <div className='ease-pay'>
-        <CardPaymentsIcon className="icon"/>
+        <CardPaymentsIcon />
       <h2>Pagamento flexible</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         
       </div>
       <div className='fast-delivery'>
-        <FastDelivery className="icon"/>
+        <FastDelivery />
       <h2>Envio rapido y seguro</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
