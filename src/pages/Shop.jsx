35c7229247d57e0f6f1824ffcfd9ab4f1f182cb7
noemi <noemibonilla95@hotmail.com>
2024-09-11 18:10:27 +0200

    implementacion de la news letter al inicio de la pagina /shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import NewProduct from '../components/NewProduct/NewProduct'
+import NewsLetter from '../components/NewsLetter/NewsLetter'
 
 export default function Shop() {
   return (
     <div>
       <Hero/>
       <NewProduct/>
+      <NewsLetter/>
     </div>
   )
 }
