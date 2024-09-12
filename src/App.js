68030d2baba07c5d471610c86baaf08b2b391387
noemi <noemibonilla95@hotmail.com>
2024-09-12 21:57:25 +0200

    agregacion de pagina blog-about-contacto hacia la app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import LoginSignUp from './pages/LoginSignUp.jsx';
 import Shop from "./pages/Shop.jsx"
 import Product from './pages/Product.jsx';
 import ShopCategory from './pages/ShopCategory.jsx';
+import About from "./pages/About.jsx"
+import Blog from "./pages/Blog.jsx"
+import Contacto from "./pages/Contacto.jsx"
+
 
 
 function App() {
@@ -18,6 +22,10 @@ function App() {
       <Navbar/> 
       <Routes>
         <Route path='/' element={<Shop/>}/>
+        <Route path='/About' element= {<About/>} />
+        <Route path='/Blog' element= {<Blog/>} />
+        <Route path='/Contacto' element= {<Contacto/>} />
+        <Route path='/productos' element= {<ShopCategory/>} />
         <Route path='/relojes-inteligentes' element={<ShopCategory category="relojes-inteligentes"/>}/>
         <Route path='/auriculares-inalambricos' element={<ShopCategory category="auriculares-inalambricos"/>}/>
         <Route path='/zapatillas-inteligentes' element={<ShopCategory category="zapatillas-inteligentes"/>}/>
