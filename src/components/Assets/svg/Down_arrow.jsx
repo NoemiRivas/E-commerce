eb379eaa0963ad2c56e35a6aa0f7d33a9288e260
noemi <noemibonilla95@hotmail.com>
2024-09-12 21:55:40 +0200

    creacion icono de flacha abajo

---
+++ src/components/Assets/svg/Down_arrow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Down_arrow() {
+  return (
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24">
+      <path d="m18.707 12.707-1.414-1.414L13 15.586V6h-2v9.586l-4.293-4.293-1.414 1.414L12 19.414z"></path></svg>
+    </div>
+  )
+}
