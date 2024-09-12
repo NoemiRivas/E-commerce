f71621c5f39b1c1076ff5f02159fa5a3cee6e4ea
noemi <noemibonilla95@hotmail.com>
2024-09-12 21:53:37 +0200

    cracion del context para la pagina de productos

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,16 @@
+import React, { createContext} from "react";
+import data_product from "../components/Assets/svg/data_product"
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+    const contextValue  = {data_product};
+    return (
+       <ShopContext.Provider value={contextValue}>
+        {props.children}
+        </ShopContext.Provider>
+    )
+        
+}
+
+export default ShopContextProvider;
